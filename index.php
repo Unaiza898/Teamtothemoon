925c95825c77dcf3b68b5812a67c1f80ef2c829e
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2022-05-15 17:01:51 -0400

    connecting to live server

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Travel app</title>
+<script src="https://kit.fontawesome.com/e22c065292.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="Dashboard.css">
+
+    <script src="/script.js" ></script>
+  </head>
+  <body>
+
+    <div class="main-container">
+    <div class="nav">
+      <div class="menu">
+        <ul class="navBar">
+         <li><img src="logo.jpg" alt="Team to the moon logo" class="logo"></li>
+         <li class="navlink"><a  href="TravelSignUp.html">Register</a></li>
+         <li class="navlink">  <a  href="TravelLogin.html">Login</a></li>
+         </ul>
+
+      </div>
+    </div>
+
+
+    <!-- <img src="images/norway.jpg" alt="" width = 100% height = 500px;> -->
+      <!-- <ul class="navBar">
+        <li><img src="images/logo.PNG" alt="Team to the moon logo" class="logo"></li>
+          <li class="navlink"><a  href="#">Register</a></li>
+          <li class="navlink">  <a  href="#">Login</a></li>
+
+        </ul> -->
+        <!-- <div class="slideshow">
+             <div class="slide-wrapper">
+                 <div class="slide"><img src="images/bora.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/swiss.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/china.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/paris.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/norway.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/europe.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/mineapolis.jpg" alt="">  </div>
+                 <div class="slide"><img src="images/pichu.jpg" alt="">  </div>
+                <div class="slide"><img src="images/seychelles.JPG" alt="">  </div>
+         </div>
+       </div> -->
+       <img src="images/norway.jpg" alt="" width = 100% height = 300px;> 
+         <div class="cont1">
+          <cite>	&#8220; Our team will take you to the moon and back for a fraction of the price&#8221;
+
+         </div>
+         <!-- logos -->
+
+
+
+         <div class="cont2">
+          <div class="tab">
+            <img src="images/flight.png" alt="flight"  width="100px" height="100px" >
+            <div class="text1">Cheaper flights and tours</div>
+          </div>
+          <div class="tab">
+            <img  src="images/discount.png" alt="discount"  width="100px" height="100px" >
+            <div class="text1">Get up to 40% discounts</div>
+          </div>
+          <div class="tab">
+            <img src="images/rewards.png" alt="rewards"   width="100px" height="100px" >
+            <div class="text1">Collect rewards for free trips</div>
+          </div>
+
+          <div class="tab">
+            <img src="images/wishlist.png" alt="wishlist"  width="90px" height="100px" >
+            <div class="text1">Make a wish list</div>
+          </div>
+          <div class="tab">
+             <img src="images/cashback.png" alt="refund"  width="80px" height="90px">
+            <div class="text1"> Get 100% refund after cancellation</div>
+          </div>
+
+        </div>
+<div class="cont3">
+    <div class="bookButton">
+      <p><a href=""></a>Book Flight</p>
+    </div>
+    <div class="bookButton">
+      <p>Book Tour</p>
+    </div>
+    <div class="bookButton">
+      <p>Reserve a hotel</p>
+    </div>
+</div>
+<footer>
+
+  <div class="content">
+    <a href="#"> Customer service </a>
+    <a href="#"> FAQ </a>
+  </div>
+  <div class="content">
+      <a href="#"> About us</a>
+    <a href="#"> Need help? </a>
+  </div>
+
+  <div class="content">
+    <a href="#"> Accessibility </a>
+    <a href="#"> User Agreement </a>
+  </div>
+  <div class="content">
+    <a href="#"> Cookie Policy</a>
+    <a href="#"> Privacy Policy </a>
+  </div>
+  <div class="content">
+    <a href="#"> Contact Us</a>
+    <div class="socialmedia">
+      <a href="https://www.facebook.com/"><i class="fab fa-facebook "></i></a>
+      <a href="https://www.instagram.com/?hl=en"><i class="fa-brands fa-instagram "></i></a>
+      <a href="https://mail.google.com/mail/u/0/#inbox"><i class="fas fa-at "></i></a>
+    </div>
+  </div>
+  <div class="copyright">
+    <p><small>&copy; Copyright 2022, TeamToTheMoon. All rights reserved. <br>NOTE: All information and materials used only for educational purposes.</small></p>
+  </div>
+</footer>
+
+
+ </div>
+
+  </body>
+</html>
